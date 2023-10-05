b45f408f8674a1cc699d4d7371720720cfdfa4fc
Mzak05 <mzakyassir@.com>
2023-10-05 04:30:56 -0700

    set

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
  *Main is for showing the infos of puts
  *we also return the value 0
  * */
+
 int main(void){
 puts("\"Programming is like building a multilingual puzzle");
 return(0);
