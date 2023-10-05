e517c83542f3222c3d97c28d11194d59be0470e0
Mzak05 <mzakyassir@.com>
2023-10-05 04:57:26 -0700

    set

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,7 @@
 *main - program that will print a string with puts
 *return: always returns 0 (succes) 
 */
-int main(void)
-{
-     puts("\"Programming is like building a multilingual puzzle");
-     return(0);
+int main(void){
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
