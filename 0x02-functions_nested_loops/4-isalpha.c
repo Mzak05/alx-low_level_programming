74dccdde0502d1cbd2dcbb5c10f744b14743f3fe
Mzak05 <mzakyassir@.com>
2023-10-10 05:32:35 -0700

    set

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
 * @c: parametrer that translates characters to their ASCII form
 * Return: 1 if true and 0 if false
 */
-int isalpha.c(int c)
+int _isalpha(int c)
 {
 if ( (c >= 65 && c <= 90) || (c >= 97 && c <= 122) )
 {
