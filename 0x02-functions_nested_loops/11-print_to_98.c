d4f4f48b686e5758b0b19ca44d86923cba76b996
Mzak05 <mzakyassir@.com>
2023-10-10 08:03:13 -0700

    set

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+* print_to_98 - print to 98
+* @n: parametrer that's used
+*/
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+for (i = n; i <= 98; i++)
+{
+if (i == 98)
+{
+printf("%d", i);
+}
+else if (i == n)
+{
+printf("%d,", i);
+}
+else
+{
+printf(" %d,", i);
+}
+}
+}
+else
+{
+for (i = n; i >= 98; i--)
+{
+if (i == 98)
+{
+printf("%d", i);
+}
+else if (i == n)
+{
+printf("%d,", i);
+}
+else
+{
+printf(" %d,", i);
+}
+}
+}
+}
