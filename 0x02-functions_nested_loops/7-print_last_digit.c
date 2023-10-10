733154febaee86ae9ad529e63845a372c2f2058e
Mzak05 <mzakyassir@.com>
2023-10-10 05:52:39 -0700

    set

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* print_last_digit - absolute value
+* @c: gets the value of the parameters
+* Return: returns postive if negative or returns the same value
+*/
+int print_last_digit(int c)
+{
+return (c % 10);
+}
