c2da6d4ceb95c6204ea34180157ef9fe3a2bbbf7
Mzak05 <mzakyassir@.com>
2023-10-10 07:15:03 -0700

    set

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,16 +7,17 @@ void times_table(void)
 int i;
 int j;
 int a;
-for (i = 0; i <10 ; i++)
+for (i = 0; i < 10 ; i++)
 {
-for (j = 0; j <=9 ; j++)
+for (j = 0; j <= 9 ; j++)
 {
 a = i * j;
-if(j == 0)
+if (j == 0)
 {
 _putchar(a + '0');
 }
-else{
+else
+{
 _putchar(' ');
 _putchar(a + '0');
 }
