efbaf2aa21bd6cf377cd56fa6b870e7a9263eda0
Mzak05 <mzakyassir@.com>
2023-10-10 05:13:50 -0700

    set

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
-* main - program that will print a string with puts
 * print_alphabet - prints alphabets
-* Return: always returns 0 (succes)
 */
 void print_alphabet(void)
 {
