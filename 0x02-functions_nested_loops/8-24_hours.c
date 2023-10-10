d7c0a0ed63513d95782d370232949feaf4de427f
Mzak05 <mzakyassir@.com>
2023-10-10 07:00:37 -0700

    set

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,10 +10,12 @@ int k;
 int l;
 for (i = 0; i < 3; i++)
 {
-if (i != 2)
-{
 for (j = 0; j < 10; j++)
 {
+if (i == 2 && j == 4)
+{
+return;
+}
 for (k = 0; k < 6; k++)
 {
 for (l = 0; l < 10; l++)
@@ -24,25 +26,6 @@ _putchar(':');
 _putchar(k + '0');
 _putchar(l + '0');
 _putchar('\n');
-}
-}
-}
-}
-else
-{
-for (j = 0; j < 4; j++)
-{
-for (k = 0; k < 6; k++)
-{
-for (l = 0; l < 10; l++)
-{
-_putchar(i + '0');
-_putchar(j + '0');
-_putchar(':');
-_putchar(k + '0');
-_putchar(l + '0');
-_putchar('\n');
-}
 }
 }
 }
