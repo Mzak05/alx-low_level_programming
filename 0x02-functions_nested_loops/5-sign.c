51aea4039af15eb0da2c7b106e3cea929415332e
Mzak05 <mzakyassir@.com>
2023-10-10 05:42:50 -0700

    set

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-* _isalpha - checks if lowercase or upercase
-* @c: parametrer that translates characters to their ASCII form
-* Return: 1 if true and 0 if false
+* print_sign - checks if lowercase or upercase
+* @n: gets the number
+* Return: 1 if pos and 0 if null and -1 if neg
 */
 int print_sign(int n)
 {
