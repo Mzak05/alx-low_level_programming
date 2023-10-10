e3d67adc171fd20da2206ff4942ea101e7403a9a
Mzak05 <mzakyassir@.com>
2023-10-10 05:50:28 -0700

    set

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * _abs - absolute value
-* @c - gets the value of the parameters
+* @c: gets the value of the parameters
 * Return: returns postive if negative or returns the same value
 */
 int _abs(int c)
