bbb55321adc607d7585617183e9a73397590a116
Mzak05 <mzakyassir@.com>
2023-10-12 11:38:19 -0700

    set

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_diagonal - print lines
+* @n: number of infos
+*/
+void print_diagonal(int n)
+{
+int i;
+for (i = 0 ; i < n ; i++)
+{
+_putchar('\\');
+_putchar('\n');
+}
+_putchar('\n');
+}
