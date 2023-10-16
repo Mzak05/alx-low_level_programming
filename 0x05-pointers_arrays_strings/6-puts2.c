6866c2686e3998617b75c16d3a3eb888f204965b
Mzak05 <mzakyassir@gmail.com>
2023-10-16 14:05:47 -0100

    set

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,9 @@
 void puts2(char *str)
 {
 int i;
-for (i = 0; str[i] != '\0'; i = i + 2)
-{
-if (str[i] != '\0')
+for (i = 0; str[i] != '\0'; i = i % 2)
 {
 _putchar(str[i]);
-}
 }
 _putchar('\n');
 }
