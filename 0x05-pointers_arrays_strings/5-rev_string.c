ebba45d83f507c48e4116bb566d2069418a96301
Mzak05 <mzakyassir@gmail.com>
2023-10-16 13:39:36 -0100

    set

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* rev_string - give reverse
+* @s: pointer
+*/
+void rev_string(char *s)
+{
+int i;
+int c;
+char o;
+c = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+c++;
+}
+for (i = c - 1; i >= 0; i--)
+{
+o = s[i];
+s[i] = s[o - i - 1];
+s[o - i - 1]=o;
+}
+}
