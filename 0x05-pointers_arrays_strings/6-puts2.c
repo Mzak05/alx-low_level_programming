5f690278e0d038ee2b0578847d95d9b919bf74ab
Mzak05 <mzakyassir@gmail.com>
2023-10-16 14:01:55 -0100

    set

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* puts2 - give length
+* @str: pointer
+*/
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i = i + 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
