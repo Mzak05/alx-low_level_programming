ba11cbe80d770317e85e4dfa438640cdd24ad41c
Mzak05 <mzakyassir@gmail.com>
2023-10-16 19:35:17 -0100

    set

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* print_array - give array
+* @a: pointer
+* @n: number of elements
+*/
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+if (i != n)
+{
+printf("%d, ", a[i]);
+}
+else
+{
+printf("%d", a[i]);
+}
+}
+}
