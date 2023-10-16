49f22c24d82f17da0c65d80ac9f11ca287eaee37
Mzak05 <mzakyassir@gmail.com>
2023-10-16 19:43:34 -0100

    set

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 /**
 * *_strcpy - give copy
 * @dest: pointer
 * @src: pointer
 * Return: length
 */
-int *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 int i;
-int c;
-c = 0;
 for (i = 0; src[i] != '\0'; i++)
 {
 dest[i] = src[i];
 }
-return (src);
+return (dest);
 }
