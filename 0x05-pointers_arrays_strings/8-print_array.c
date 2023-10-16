05459041569688494d99dafc8f877b24e495c144
Mzak05 <mzakyassir@gmail.com>
2023-10-16 19:36:58 -0100

    set

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,7 +9,7 @@ void print_array(int *a, int n)
 int i;
 for (i = 0; i < n; i++)
 {
-if (i != n)
+if (i != n-1)
 {
 printf("%d, ", a[i]);
 }
@@ -18,4 +18,5 @@ else
 printf("%d", a[i]);
 }
 }
+printf("\n");
 }
