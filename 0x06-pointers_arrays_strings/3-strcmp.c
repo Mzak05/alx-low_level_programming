56356fb41f7efbf46921551e4d0435562054fa13
Mzak05 <mzakyassir@.com>
2023-10-18 11:55:07 -0700

    set

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,11 @@ int _strcmp(char *s1, char *s2)
 {
 int i;
 i = 0;
-while(s1[i] != '\0' && s2[i] != '\0')
+while (s1[i] != '\0' && s2[i] != '\0')
 {
 if (s1[i] != s2[i])
 {
-return s1[i] - s2[i];
+return (s1[i] - s2[i]);
 }
 else
 {
@@ -22,14 +22,14 @@ i++;
 }
 if (s1[i] == '\0' && s2[i] == '\0')
 {
-return 0;
+return (0);
 }
 else if (s1[i] == '\0')
 {
-return -s2[i];
+return (-s2[i]);
 }
 else
 {
-return s1[i];
+return (s1[i]);
 }
 }
