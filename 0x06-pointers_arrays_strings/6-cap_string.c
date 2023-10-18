631d424c55d1d23f810710eeb28710e06110e4cd
Mzak05 <mzakyassir@.com>
2023-10-18 12:38:27 -0700

    set

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,25 +4,22 @@
 * @c:pointer
 * Return: result
 */
-char *cap_string(char *c)
+char *cap_string(char *str)
 {
 int i = 0;
-int f = 1;
 while (str[i])
 {
 if (str[i] >= 'a' && str[i] <= 'z')
 {
-if (f)
+if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' ||
+str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' ||
+str[i - 1] == '{' || str[i - 1] == '}')
 {
 str[i] -= 32;
-should_capitalize = 0;
 }
-}
-else
-}      
-f= 1;
 }
 i++;
 }
-return str;
+return (str);
 }
