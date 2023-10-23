2c730a9bde0a89eb969f3585181e37f6f53a7a9d
Mzak05 <mzakyassir@.com>
2023-10-23 12:54:28 -0700

    set

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,19 +12,12 @@ int i;
 int j;
 for (i = 0; s[i] != '\0'; i++)
 {
-if (s[i] != ' ')
-{
 for (j = 0; accept[j] != '\0'; j++)
 {
 if (s[i] == accept[j])
 {
 return (&s[i]);
 }
-}
-}
-else
-{
-break;
 }
 }
 return (NULL);
