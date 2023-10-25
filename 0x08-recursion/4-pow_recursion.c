45b6b95045db26fd03782d18ece8c716767f0375
Mzak05 <mzakyassir@.com>
2023-10-25 03:32:24 -0700

    set

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _pow_recursion - give n values
+* @x: value
+* @y: value
+* Return: value
+*/
+int _pow_recursion(int x, int y)
+{
+if (y > 0)
+{
+return (x * _pow_recursion(x , y - 1));
+}
+else
+{
+return (1);
+}
+}
