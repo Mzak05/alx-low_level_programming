87948d270bd3eb6c9d8aedcc1f2bd66edb19bdee
Mzak05 <mzakyassir@.com>
2023-10-25 03:53:35 -0700

    set

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,25 @@
 #include "main.h"
 /**
 * _sqrt_recursion - give sqrt
+* _sql - give sqrt
 * @n: number
+* @n: number
+* @a: number
 * Return: value
 */
 int _sql(int n, int a)
 {
 if (a * a == n)
 {
-return a;
+return (a);
 }
 else if (a * a > n)
 {
-return -1;
+return (-1);
 }
-return _sql(n, a + 1);
+return (_sql(n, a + 1));
 }
 int _sqrt_recursion(int n)
 {
-return _sql(n, 1);
+return (_sql(n, 1));
 }
