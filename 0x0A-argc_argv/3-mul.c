19405ab39b728b71b2b82db4e01904d3ce0eea69
Mzak05 <mzakyassir@.com>
2023-10-27 13:57:53 -0700

    set

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print infos
+ * @argc: data
+ * @argv: data
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+if (argc < 2)
+{
+printf("Error\n");
+return (1);
+}
+int a = atoi(argv[1]);
+int b = atoi(argv[2]);
+printf("%d\n", a * b);
+return (0);
+}
