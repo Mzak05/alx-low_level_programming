05cd2c0d099a19c7acf4c004de43f5f7c9ceed65
Mzak05 <mzakyassir@.com>
2023-10-30 11:53:23 -0700

    set

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+* free_grid - give data
+* @grid: data
+* @height: s2
+*/
+void free_grid(int **grid, int height)
+{
+int i;
+if (grid == NULL || height < 0)
+{
+return;
+}
+for (i = 0; i < height; i++)
+{
+free(grid[i]);
+}
+}
