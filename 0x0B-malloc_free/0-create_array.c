4282e47276df1c125b099517f5876161ca9407d2
Mzak05 <mzakyassir@.com>
2023-10-30 05:23:12 -0700

    set

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* *create_array - give data
+* @size: size
+* @c: char
+* Return: return
+*/
+char *create_array(unsigned int size, char c)
+{
+if (size == 0)
+{
+return (NULL);
+}
+else 
+{
+
+}
+}
