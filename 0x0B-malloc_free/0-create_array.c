385c51324e5c28c3cea05ad7cfee7606932beffe
Mzak05 <mzakyassir@.com>
2023-10-30 05:30:14 -0700

    set

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 /**
 * *create_array - give data
 * @size: size
@@ -7,12 +8,26 @@
 */
 char *create_array(unsigned int size, char c)
 {
+unsigned int i;
+char *t = (char *)malloc(sizeof(char) * size);
 if (size == 0)
 {
 return (NULL);
 }
 else 
 {
-
+if (t == NULL)
+{
+return (NULL);
 }
+else
+{
+for (i = 0; i < size; i++)
+{
+t[i] = c;
+}
+return (t);
+}
+}
+return (NULL);
 }
