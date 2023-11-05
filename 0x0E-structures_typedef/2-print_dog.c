8b9ee5f9b0cacfb9d1aa0fbc31b2494d5297bbd9
Mzak05 <mzakyassir@.com>
2023-11-05 10:30:47 -0800

    set

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,10 +8,24 @@
 */
 void print_dog(struct dog *dogo)
 {
-if (dogo != NULL)
-{
+if (dogo != NULL) {
+if (dogo->name != NULL) {
 printf("Name: %s\n", dogo->name);
+}
+else {
+printf("Name: (nil)\n");
+}
+if (dogo->age != 0) {
 printf("Age: %f\n", dogo->age);
+}
+else {
+printf("Age: 0\n");
+}
+if (dogo->owner != NULL) {
 printf("Owner: %s\n", dogo->owner);
+}
+else {
+printf("Owner: (nil)\n");
+}
 }
 }
