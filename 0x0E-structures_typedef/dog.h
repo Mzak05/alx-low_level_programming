706804b696f5e509152d304c716af8a529835f7f
Mzak05 <mzakyassir@.com>
2023-11-05 10:14:53 -0800

    set

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - dog infos
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
