28316c53a40080a070371d023dfc0be2cbf7d028
Mzak05 <mzakyassir@.com>
2023-11-08 13:01:41 -0800

    set

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,26 +1,38 @@
-#include "3-calc.h"
-#include <stdlib.h>
+#include "function_pointers.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "3-calc.h"
 /**
-* main - fct
-* @argc: fct
-* @argv: fct
+* main - info of main
+* @argc: data
+* @argv: data
 * Return: return
 */
 int main(int argc, char *argv[])
 {
-int (*info)(int, int);
+int no, np;
+int result;
+char *op;
 if (argc != 4)
 {
 printf("Error\n");
-exit(98);
+return (98);
 }
-info = get_op_func(argv[2]);
-if (info == NULL)
+no = atoi(argv[1]);
+op = argv[2];
+np = atoi(argv[3]);
+if (strlen(op) != 1 || get_op_func(op) == NULL)
 {
 printf("Error\n");
-exit(99);
+return (99);
 }
-printf("%d\n", info(atoi(argv[1]), atoi(argv[3])));
+if ((*op == '/' || *op == '%') && np == 0)
+{
+printf("Error\n");
+return (100);
+}
+result = get_op_func(op)(no, np);
+printf("%d\n", result);
 return (0);
 }
