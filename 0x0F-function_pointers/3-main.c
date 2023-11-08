08ce86206bd192b794cda493484adde37b1489a9
Mzak05 <mzakyassir@.com>
2023-11-08 12:36:37 -0800

    set

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,18 +9,19 @@
 */
 int main(int argc, char *argv[])
 {
-int (*info)(int, int);
-info = get_op_func(argv[2]);
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
-if (info == NULL)
+if (argv[2] == NULL || get_op_func(argv[2]) == NULL)
 {
 printf("Error\n");
 exit(99);
 }
-printf("%d\n", info(atoi(argv[1]), atoi(argv[3])));
+int (*info)(int, int);
+info = get_op_func(argv[2]);
+printf("%d\n", info(atoi(argv[1]), atoi(argv[3]));
 return (0);
 }
+
