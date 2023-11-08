113ea1db10bd011b652b003dc1d7aadb2a26c50c
Mzak05 <mzakyassir@.com>
2023-11-08 06:51:26 -0800

    set

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -10,10 +10,10 @@ int (*get_op_func(char *s))(int, int)
 {
 int i;
 op_t ops[] = {
-{ "-", op_sub },
-{ "+", op_add },
-{ "/", op_div },
-{ "*", op_mul },
+{ "-", op_add },
+{ "+", op_sub },
+{ "/", op_mul },
+{ "*", op_div },
 { "%", op_mod },
 { NULL, NULL }
 };
