30d6b7962d1ada19f9d3f3415b3347bcc3645d05
Mzak05 <mzakyassir@.com>
2023-11-08 06:05:17 -0800

    set

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,12 +9,12 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
-if (size <=0 || array == NULL || cmp == NULL)
+if (size <= 0 || array == NULL || cmp == NULL)
 {
 return (-1);
 }
 i = 0;
-while( i < size)
+while (i < size)
 {
 if (cmp(array[i] == 1))
 {
