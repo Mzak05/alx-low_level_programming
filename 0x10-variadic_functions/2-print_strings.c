9654965689ead2b5c7eedd2242c7e452a8961333
Mzak05 <mzakyassir@.com>
2023-11-09 12:17:27 -0800

    set

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+if (n > 0)
+{
+va_list args;
+char *current_str;
+unsigned int index;
+index = 1;
+current_str = va_arg(args, char *);
+va_start(args, n);
+printf("%s", va_arg(args, char *));
+while (index < n)
+{
+char *current_str = va_arg(args, char *);
+if (!current_str)
+{
+current_str = "(nil)";
+}
+if (separator)
+{
+printf("%s%s", separator, current_str);
+}
+else
+{
+printf("%s", current_str);
+}
+index++;
+}
+va_end(args);
+}
+printf("\n");
+}
