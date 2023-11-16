30666f591098e13ed2b72b189bb776b7dc28e234
Mzak05 <mzakyassir@.com>
2023-11-16 13:57:12 -0800

    set

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,25 +9,24 @@ size_t print_list(const list_t *h)
 const list_t *pp;
 size_t c;
 c = 0;
-pp = h;
-if (pp == NULL)
+if (h == NULL)
 {
 return (0);
 }
 else
 {
-while (pp != NULL)
+while (h != NULL)
 {
-if (pp->str == NULL)
+if (h->str == NULL)
 {
 printf("[%d] %s\n", 0, "(nil)");
 }
 else
 {
-printf("[%d] %s\n", pp->len, pp->str);
+printf("[%d] %s\n", h->len, h->str);
 }
 c += 1;
-pp = pp->next;
+h = h->next;
 }
 }
 return (c);
