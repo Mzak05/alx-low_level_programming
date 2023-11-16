f51d2733f68e0017b72e771f203f2429ad8d1eb8
Mzak05 <mzakyassir@.com>
2023-11-16 14:12:36 -0800

    set

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,35 +1,22 @@
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
 /**
-* print_list - info
-* @h: info
-* Return: Num
+* print_list - data
+* @h: datas
+* Return: return
 */
 size_t print_list(const list_t *h)
 {
-const list_t *pp;
-size_t c;
-c = 0;
-pp = h;
-if (pp == NULL)
+size_t c = 0;
+const list_t *pp = h;
+while (pp)
 {
-return (0);
-}
-else
-{
-while (pp != NULL)
-{
-if (pp->str == NULL)
-{
+if (!pp->str)
 printf("[0] (nil)\n");
-}
 else
-{
 printf("[%u] %s\n", pp->len, pp->str);
-}
-c += 1;
 pp = pp->next;
-}
+c++;
 }
 return (c);
 }
