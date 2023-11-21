aea73ec243a272029ce66c86be45f064e004093e
Mzak05 <mzakyassir@.com>
2023-11-21 10:59:02 -0800

    set

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * free_listint2 - frees a linked list
- * @head: pointer to the listint_t list to be freed
+ * free_listint2 - free
+ * @head: point
  */
 void free_listint2(listint_t **head)
 {
